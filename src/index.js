278216318ff4fba27db2e22c38eead66dcef944f
Leonardo Costa <leonardo.fc2@outlook.com>
2019-07-29 13:55:23 -0300

    rename app folder to src folder

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require ('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-const apiRoutes = require('./app/routes');
+const apiRoutes = require('./routes');
 
 const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
-app.use('/api', apiRoutes);
+app.use('/src', apiRoutes);
 app.use(express.static('public'));
 
 const port = process.env.PORT || 3333;
