ce965e6bb47b7fe260e0ea6a8055865c00f5e80b
Yuri Faria <yuri.yurifaria@gmail.com>
2019-07-29 22:23:29 -0300

    controller validation

--- app/controllers/SlideController.js
+++ app/controllers/SlideController.js
@@ -9,8 +9,14 @@ const robots = {
 const controller = {};
 
 controller.post = async (req, res) => {
-    let content = { lang, author, searchTerm, font, prefix, numberOfSlides } = req.body;
+    let content = { lang = 'en', author = '', searchTerm, font = 'Roboto', prefix, numberOfSlides = 7 } = req.body;
     content.id = generateRandomId();
+
+    if(!searchTerm)
+      return res.status(400).json({ error: 'search term missing' });
+
+    if(numberOfSliddes > 20)
+      return res.status(400).json({ error: 'number of slides exceeded the limit' });
 
     try {
       content = await robots.text.start(content);
@@ -18,7 +24,7 @@ controller.post = async (req, res) => {
       await robots.powerPoint.start(content);
       res.json({ slideId: content.id });
     } catch(error) {
-      res.status(400).json({ error });
+      res.status(500).json({ error });
     }
 }
 
